e4266b95787a189e8bf91c6969ce6a6187ec3fe3
Karin1993 <daegumxxw@gmail.com>
2023-12-03 21:07:50 -0600

    First commit

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+  resultado = 0;
+  numero1 = 0;
+  numero2 = 0;
+}
